b08eb0220b458a1617a77ad4bb7781a712d429e6
Jeuan Mitchell <jeuan.mitchell@gmail.com>
2025-08-23 16:44:24 -0500

    variables and constants

---
+++ scripts/script.js
@@ -0,0 +1,78 @@
+console.log("My script is working!");
+
+
+// String value
+let firstName = "Jeuan";
+// Numerical value
+let myAge = 54;
+let isAprofessor = false;
+let isAstudent = true;
+//display values in console
+console.log(firstName);
+console.log( myAge);
+console.log( isAprofessor);
+console.log( isAstudent);
+// create a variable for your last Name
+let lastName = "Mitchell";
+
+//create a variable for your height
+let myheight = 5.7;
+
+//finally show each variable separately in the console
+console.log(lastName);
+console.log(myheight);
+console.log("My name is " + firstName + " " + lastName + " and I am " + myAge + " years old.");
+console.log("My name is " + firstName + " " + lastName + " and I am " + myAge + " years old." + " My height is " + myheight + " feet tall.");
+
+let num1=10;
+let num2=2;
+let addition=num1+num2;
+let subtraction=num1-num2;
+let multiplication=num1*num2;
+let division=num1/num2;
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log("These are the fundamental arithmetic operations for" +num1 + " and " + num2);
+console.log("Addition of " + num1 + " and " + num2 + " is " + addition);
+console.log("Subtraction of " + num1 + " and " + num2 + " is " + subtraction);
+console.log("Multiplication of " + num1 + " and " + num2 + " is " + multiplication);
+console.log("Division of " + num1 + " and " + num2 + " is " + division);
+let radius=3;
+let pi= 3.1416;
+let areaOfaCircle=pi * radius * radius;
+
+console.log("Area of the circle with a radius of " + radius + " is " + areaOfaCircle);
+let integerNumber=1625;
+console.log("The value of integerNumber is: " + integerNumber);
+integerNumber=375;
+let newVariable=integerNumber+125;
+console.log("The value of newVariable is 375+125 : " + newVariable);
+
+//Difference between variable and constants
+// Definition of a variable
+let aVariable;
+//Assign a value
+aVariable = 20;
+
+// Define and initialize at the same time
+let otherVariable = 30;
+console.log(aVariable, otherVariable);
+// Changing the value of a variable and a const
+aVariable = 50;
+otherVariable=100;
+console.log(aVariable, otherVariable);
+//const weekDays=7;
+//console.log(weekDays);
+weekDays=10;
+console.log(weekDays);
+//daysInWeek
+const daysInWeek = 7;
+let vacationDays = 14;
+let totalDays = (daysInWeek + vacationDays);
+console.log(totalDays);
+vacationDays=30;
+totalDays = (daysInWeek + vacationDays);
+console.log("Total days of vacation after update " + totalDays);
+
